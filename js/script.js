eed540ed254cbc20217d80801cb99f49c105264d
flloki97 <fbryma7@gmail.com>
2024-05-06 18:31:39 +0200

    adding sound animation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const audio = document.getElementById('audioPlayer');
-  const soundIcon = document.getElementById('soundIcon');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const audio = document.getElementById('audioPlayer');
+//   const soundIcon = document.getElementById('soundIcon');
 
-  // Function to toggle sound
-  function toggleSound() {
-    if (audio.paused) {
-      audio.play();
-      soundIcon.src = 'img/sound-waves.png';
-    } else {
-      audio.pause();
-      soundIcon.src = 'img/sound-waves.png';
-    }
-  }
+//   // Function to toggle sound
+//   function toggleSound() {
+//     if (audio.paused) {
+//       audio.play();
+//       soundIcon.src = 'img/sound-waves.png';
+//     } else {
+//       audio.pause();
+//       soundIcon.src = 'img/sound-waves.png';
+//     }
+//   }
 
-  // Toggle sound when button is clicked
-  document.getElementById('toggleButton').addEventListener('click', toggleSound);
-});
+//   // Toggle sound when button is clicked
+//   document.getElementById('toggleButton').addEventListener('click', toggleSound);
+// });
