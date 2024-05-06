d09bb0734d349310d6bb95324e90a96129518254
flloki97 <fbryma7@gmail.com>
2024-05-06 16:45:34 +0200

    adding something

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const audio = document.getElementById('audioPlayer');
+  const soundIcon = document.getElementById('soundIcon');
+
+  // Function to toggle sound
+  function toggleSound() {
+    if (audio.paused) {
+      audio.play();
+      soundIcon.src = 'img/sound-waves.png';
+    } else {
+      audio.pause();
+      soundIcon.src = 'img/sound-waves.png';
+    }
+  }
+
+  // Toggle sound when button is clicked
+  document.getElementById('toggleButton').addEventListener('click', toggleSound);
+});
