e3715a05ff22ea5ef7a40b289c2805e13e64e32c
flloki97 <fbryma7@gmail.com>
2024-05-08 16:17:40 +0200

    responsive design

--- js/script.js
+++ js/script.js
@@ -137,4 +137,14 @@ $('.menu').click(function() {
   setTimeout(function() {
     $('.mobile-menu ul').toggleClass('open-menu');
   }, 200); // Adjust delay as needed to match transition timing
+
+  if ($('.mobile-menu ul').hasClass('open-menu')) {
+    // Apply transition delays to each list item
+    $('.mobile-menu ul.open-menu li a').each(function(index) {
+      $(this).css('transition-delay', (index * 0.6) + 's');
+    });
+  } else {
+    // If the menu is closed, reset transition delays
+    $('.mobile-menu ul li a').css('transition-delay', '');
+  }
 });
